13af0afb5304389d0872e0eb6a023b54072ad381
Musharraf2000 <musharraf.azhar19@gmail.com>
2026-03-19 09:29:13 +0530

    Created Book Class

---
+++ Week2COM25A1/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2COM25A1
+{
+    internal class Book
+    {
+        public string title;
+        public string author;
+    }
+}
--- Week2COM25A1/Program.cs
+++ Week2COM25A1/Program.cs
@@ -1,15 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Week2COM25A1;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 
 
-int number = 5;
+//int number = 5;
 
 
-Student s1 = new Student();
-s1.name = "John Doe";
-s1.age = 20;
-s1.memberId = 1;
-s1.phoneNumber = "123-456-7890";
+//Student s1 = new Student();
+//s1.name = "John Doe";
+//s1.age = 20;
+//s1.memberId = 1;
+//s1.phoneNumber = "123-456-7890";
+
+Book b1 = new Book();
+b1.title = "The Great Gatsby";
+b1.author = "F. Scott Fitzgerald";
+
+Console.WriteLine($"The book {b1.title} was written by {b1.author}");
